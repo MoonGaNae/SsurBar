b269998e673a56bf9f0a9de8bc344508dd8fb799
MoonGaNae <anstjd9115@gmail.com>
2021-10-22 17:00:00 +0900

    init: backend 프로젝트 생성

---
+++ backend/src/test/java/com/ssurbar/survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssurbar.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
