6aa25057b1f106e0514c62bbc1aabdcb65807cb9
Daun jo <jodawooooon@naver.com>
2021-10-25 13:21:57 +0900

    init: 백엔드 프로젝트 구조 설정

---
+++ backend/src/main/java/com/ssurbar/api/service/Test.java
@@ -0,0 +1,4 @@
+package com.ssurbar.api.service;
+
+public class Test {
+}
---
+++ backend/src/main/java/com/ssurbar/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.ssurbar.config;
+
+
+/**
+ * API 문서 관련 swagger2 설정 정의.
+ */
+
+public class SwaggerConfig {
+
+}
