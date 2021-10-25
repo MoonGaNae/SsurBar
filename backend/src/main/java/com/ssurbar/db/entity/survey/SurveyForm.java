bfeadd7d0fb2f0ceac48e6290503c1b350721f5b
MoonGaNae <anstjd9115@gmail.com>
2021-10-25 15:04:07 +0900

    wip : ServeyForm Entity 생성

---
+++ backend/src/main/java/com/ssurbar/db/entity/survey/SurveyForm.java
@@ -0,0 +1,24 @@
+package com.ssurbar.db.entity.survey;
+
+import com.ssurbar.db.entity.survey.Survey;
+import lombok.Builder;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+public class SurveyForm implements Serializable {
+
+    @Id
+    private String surveyFormId;
+
+    private String title;
+
+    private String desc;
+    
+}
