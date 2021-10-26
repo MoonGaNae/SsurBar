8c7ddcacdfc58d4899dfc86f3dbb52331813e9b0
조현민 <hyeonmean@gmail.com>
2021-10-26 11:32:17 +0900

    feat: 전체 설문 목록 불러오기

--- backend/src/main/java/com/ssurbar/db/repository/survey/SurveyRepository.java
+++ backend/src/main/java/com/ssurbar/db/repository/survey/SurveyRepository.java
@@ -1,10 +1,16 @@
 package com.ssurbar.db.repository.survey;
 
-import com.ssurbar.db.entity.survey.Survey;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ssurbar.db.entity.survey.Survey;
+import com.ssurbar.db.entity.survey.SurveyForm;
+
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, String> {
+
+	List<Survey> findAllBySurveyFormId(SurveyForm surveyForm);
 
 }
