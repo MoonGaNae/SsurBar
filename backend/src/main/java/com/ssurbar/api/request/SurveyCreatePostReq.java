5157f23bbb43c1542e5da7fbcf40e6a66956f85f
조현민 <hyeonmean@gmail.com>
2021-10-26 11:31:40 +0900

    feat: 설문 생성 요청 dto

---
+++ backend/src/main/java/com/ssurbar/api/request/SurveyCreatePostReq.java
@@ -0,0 +1,22 @@
+package com.ssurbar.api.request;
+
+import java.util.Date;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@ApiModel("SurveyCreatePostReq")
+public class SurveyCreatePostReq {
+	@ApiModelProperty(name="surveyFormId", example="")	
+	private String surveyFormId;
+	@ApiModelProperty(name="creationTime", example="")
+	private Date creationTime;
+	@ApiModelProperty(name="endTime", example="")
+	private Date endTime;
+}
