b9987b1073c8b95ebae0491d3c81ade432e05f96
MoonGaNae <anstjd9115@gmail.com>
2021-10-28 10:35:46 +0900

    fix : TemplatePostReq builder 어노테이션 추가

--- backend/src/main/java/com/ssurbar/survey/api/request/TemplatePostReq.java
+++ backend/src/main/java/com/ssurbar/survey/api/request/TemplatePostReq.java
@@ -2,12 +2,14 @@ package com.ssurbar.survey.api.request;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @ApiModel("TemplatePostReq")
+@Builder
 public class TemplatePostReq {
     @ApiModelProperty(name="title", example="")
     private String title;
