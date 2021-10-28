539bcd39fcf24f3460516ea36da6f6619f3b50c5
조현민 <hyeonmean@gmail.com>
2021-10-28 11:01:42 +0900

    [#3] 설문 링크 생성

--- backend/src/main/java/com/ssurbar/survey/api/service/SurveyServiceImpl.java
+++ backend/src/main/java/com/ssurbar/survey/api/service/SurveyServiceImpl.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
 
 import com.ssurbar.survey.api.request.SurveyCreatePostReq;
 import com.ssurbar.survey.api.response.SurveyInfo;
+import com.ssurbar.survey.common.util.LinkUtil;
 import com.ssurbar.survey.common.util.RandomIdUtil;
 import com.ssurbar.survey.db.entity.Team;
 import com.ssurbar.survey.db.entity.survey.Survey;
 import com.ssurbar.survey.db.entity.survey.SurveyForm;
 import com.ssurbar.survey.db.entity.survey.SurveyResponseLog;
-import com.ssurbar.survey.db.entity.survey.SurveyTarget;
 import com.ssurbar.survey.db.repository.survey.SurveyRepository;
 import com.ssurbar.survey.db.repository.survey.SurveyResponseLogRepository;
 import com.ssurbar.survey.db.repository.survey.SurveyTargetRepository;
@@ -36,6 +36,9 @@ public class SurveyServiceImpl implements SurveyService {
     
     @Autowired
     RandomIdUtil randomIdUtil;
+    
+    @Autowired
+    LinkUtil linkUtil;
 
     /* 새로운 설문지 생성 */
     @Override
@@ -45,45 +48,14 @@ public class SurveyServiceImpl implements SurveyService {
     	List<Team> teamList = teamRepository.findAllById(surveyCreatePostReq.getTeamIdList());
     	
     	
-    	List<SurveyTarget> surveyTargetList = surveyTargetRepository.findAll();
+//    	List<SurveyTarget> surveyTargetList = surveyTargetRepository.findAll();
     	List<Survey> surveyList = surveyRepository.findAll();
-    	/*------------------ survey target 데이터 생성 및 저장 시작  -----------------------*/
     	
     	for (Team team : teamList) {
-    		String surveyTargetId = randomIdUtil.makeRandomId(13);
-    		
-    		boolean isSurveyTargetIdExist = false;
-    		
-    		while(true) {
-    			for (SurveyTarget surveyTarget : surveyTargetList) {
-    				if(surveyTarget.getSurveyTargetId().equals(surveyTargetId)) {
-    					isSurveyTargetIdExist = true;
-    					break;
-    				} 
-    			}
-    			
-    			if(!isSurveyTargetIdExist) {
-    				break;
-    			}
-    			
-    			surveyTargetId = randomIdUtil.makeRandomId(13);
-    			isSurveyTargetIdExist = false;
-    		}
-    		
-    		SurveyTarget savedSurverTarget = surveyTargetRepository.save(SurveyTarget.builder()
-	    		.surveyTargetId(surveyTargetId)
-	    		.team(team)
-	    		.responseUrl("testResUrl")
-	    		.resultUrl("testResultUrl")
-	    		.build());
-    		
-			surveyTargetList.add(savedSurverTarget);
-			
 	    	/*------------------ survey 데이터 생성 및 저장 시작  -----------------------*/
 	    	String surveyId = randomIdUtil.makeRandomId(13);
 	    	
 	    	boolean isSurveyIdExist = false;
-	    	
 	    	
 	    	while(true) {
 	    		for (Survey survey : surveyList) {
@@ -101,53 +73,23 @@ public class SurveyServiceImpl implements SurveyService {
 	    		isSurveyIdExist = false;
 	    	}
 	    	
+	    	String responseUrl = linkUtil.makeUrl(surveyId, "response");
+	    	String resultUrl = linkUtil.makeUrl(surveyId, "result");
+	    	
 	        Survey savedSurvey = surveyRepository.save(Survey.builder()
 	        		.surveyId(surveyId)
 	        		.surveyForm(surveyForm)
 	        		.creationTime(surveyCreatePostReq.getCreationTime())
 	        		.endTime(surveyCreatePostReq.getEndTime())
-	        		.surveyTarget(savedSurverTarget)
+	        		.team(team)
+	        		.responseUrl(responseUrl)
+	        		.resultUrl(resultUrl)
 	        		.build());
 	        
 	        surveyList.add(savedSurvey);
 	        /*------------------ survey 데이터 생성 및 저장 끝  -----------------------*/
-	        
 		}
-    	/*------------------ survey target 데이터 생성 및 저장 끝  -----------------------*/
     	
-//    	/*------------------ survey 데이터 생성 및 저장 시작  -----------------------*/
-//    	String surveyId = randomIdUtil.makeRandomId(13);
-//    	
-//    	boolean isSurveyIdExist = false;
-//    	
-//    	List<Survey> surveyList = surveyRepository.findAll();
-//    	
-//    	while(true) {
-//    		for (Survey survey : surveyList) {
-//    			if(survey.getSurveyId().equals(surveyId)) {
-//    				isSurveyIdExist = true;
-//    				break;
-//    			} 
-//			}
-//    		
-//    		if(!isSurveyIdExist) {
-//    			break;
-//    		}
-//    		
-//    		surveyId = randomIdUtil.makeRandomId(13);
-//    		isSurveyIdExist = false;
-//    	}
-//    	
-//        Survey survey = Survey.builder()
-//        		.surveyId(surveyId)
-//        		.surveyForm(surveyForm)
-//        		.creationTime(surveyCreatePostReq.getCreationTime())
-//        		.endTime(surveyCreatePostReq.getEndTime())
-//        		.build();
-//        
-//        surveyRepository.save(survey);
-//        /*------------------ survey 데이터 생성 및 저장 끝  -----------------------*/
-
         return true;
     }
 
@@ -164,7 +106,7 @@ public class SurveyServiceImpl implements SurveyService {
 					.endTime(survey.getEndTime())
 					.surveyId(survey.getSurveyId())
 					.title(survey.getSurveyForm().getTitle())
-					.teamName(survey.getSurveyTarget().getTeam().getName())
+					.teamName(survey.getTeam().getName())
 					.build());
 		}
 		
@@ -175,7 +117,7 @@ public class SurveyServiceImpl implements SurveyService {
 	public int getSurveyResponseCount(String surveyId) {
 		Survey survey = surveyRepository.findById(surveyId).orElse(null);
 		
-		List<SurveyResponseLog> list = surveyResponseLogRepository.findAllBySurveyTarget(survey.getSurveyTarget());
+		List<SurveyResponseLog> list = surveyResponseLogRepository.findAllBySurvey(survey);
 		
 		return list.size();
 	}
