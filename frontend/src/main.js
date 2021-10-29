a6d8d85670ae7f73f7eb9c5fac456a47735aa2e1
kangazy7 <hg_kang7@Naver.com>
2021-10-29 15:01:25 +0900

    [#6] feat: 중간점검

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import Element from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(Element);
-
+ 
 Vue.config.productionTip = false;
 
 new Vue({
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// 설문 제작 
+import Editform from "../views/Editform.vue";
+// 설문 문항 편집
+import Createform from "../views/Createform.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/Editform",
+    name: "Editform",
+    component: Editform,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/Createform",
+    name: "Createform",
+    component: Createform,
   },
+  
 ];
 
 const router = new VueRouter({
