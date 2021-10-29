9027568aad98a5ea492dc2431f302568bcd5fbdf
hyewonyy <hyywon97@gmail.com>
2021-10-29 14:37:38 +0900

    [#9] wip: 응답페이지 UI 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,18 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/form">Survey</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
+
+
 <style>
+ @import './assets/style/main.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
