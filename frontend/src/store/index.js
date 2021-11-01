1a482e3dd7493d93b7f599a0d648434656f56dcc
조현민 <hyeonmean@gmail.com>
2021-11-01 18:00:32 +0900

    [#16] feat: 프론트엔드 template, survey store 작성

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import question from "@/store/modules/question.js";
+import template from "@/store/modules/template.js";
+import survey from "@/store/modules/survey.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { question },
+  modules: { question, template, survey },
   state: {},
   mutations: {},
   actions: {},
