5665040e816dcaf93bf5c0212f1ae3dd61f6ffd2
DESKTOP-KVCQHCD\multicampus <hyeonmean@gmail.com>
2021-11-04 16:34:42 +0900

    [#27] feat: 필터 기본 UI 및 동작

--- frontend/src/views/survey/SurveyDetail.vue
+++ frontend/src/views/survey/SurveyDetail.vue
@@ -18,21 +18,84 @@
           <div class="content-container" :class="{ isFullContent: checkFullContent }"> -->
           <div id="tab-div">
             <ul class="tabs">
-              <li class="tab" :class="{ selectedTab: selectedTabNum == 0 }" @click="changeTab(0)">
+              <li
+                class="tab"
+                :class="{ selectedTab: selectedTabNum == 0 }"
+                @click="changeTab(0)"
+              >
                 분석
               </li>
               <!-- <li class="tab">비교</li> -->
-              <li class="tab" :class="{ selectedTab: selectedTabNum == 2 }" @click="changeTab(2)">
+              <li
+                class="tab"
+                :class="{ selectedTab: selectedTabNum == 2 }"
+                @click="changeTab(2)"
+              >
                 배포
               </li>
-              <li class="tab" :class="{ selectedTab: selectedTabNum == 3 }" @click="changeTab(3)">
+              <li
+                class="tab"
+                :class="{ selectedTab: selectedTabNum == 3 }"
+                @click="changeTab(3)"
+              >
                 결과
               </li>
             </ul>
           </div>
           <div class="detail-container">
-            <div class="filter-container el-card is-always-shadow" v-if="!checkFullContent">
-              sdadasdsad
+            <div
+              class="filter-container el-card is-always-shadow"
+              v-if="!checkFullContent"
+            >
+              <div>필터</div>
+              <!-- <div class="filter-div"> -->
+              <div class="filter-list">
+                <div
+                  class="filte-div"
+                  v-for="(filter, filterIdx) in filterList"
+                  :key="filterIdx"
+                >
+                  <div class="filter el-card is-always-shadow">
+                    <div
+                      class="filter-title"
+                      @click="clickFilterDiv(filterIdx)"
+                    >
+                      {{ filter.name }}
+
+                      <i
+                        v-if="filter.isSelected"
+                        class="el-icon-arrow-down"
+                      ></i>
+                      <i v-else class="el-icon-arrow-left"></i>
+                    </div>
+                    <div class="filter-content" v-if="filter.isSelected">
+                      <div
+                        v-for="(filterName, nameIdx) in filter.filterNames"
+                        :key="nameIdx"
+                      >
+                        <label :for="filter + filterName">{{
+                          filterName
+                        }}</label>
+                        <input
+                          type="checkbox"
+                          :id="filter + filterName"
+                          :value="filterName"
+                          v-model="checkedFilter[filterIdx].filterValue"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- </div> -->
+              </div>
+              <div class="button-div">
+                <button
+                  @click="applyFilter()"
+                  class="yellow-button rounded-corner-button"
+                >
+                  적용하기
+                </button>
+              </div>
             </div>
             <div
               class="content-container el-card is-always-shadow"
@@ -48,7 +111,10 @@
                 <SurveyResult :surveyId="surveyId" />
               </div>
             </div>
-            <div class="feedback-container el-card is-always-shadow" v-if="!checkFullContent">
+            <div
+              class="feedback-container el-card is-always-shadow"
+              v-if="!checkFullContent"
+            >
               12321323
             </div>
           </div>
@@ -61,7 +127,7 @@
 </template>
 
 <script>
-// import axios from "@/utils/axios.js";
+import axios from "@/utils/axios.js";
 // import { mapActions, mapGetters } from "vuex";
 import SurveyRealease from "@/components/detail/SurveyRealease.vue";
 import SurveyAnalysis from "@/components/detail/SurveyAnalysis.vue";
@@ -77,12 +143,37 @@ export default {
   data() {
     return {
       selectedTabNum: 0,
+      checkedFilter: [],
       surveyId: "samplesurvey1",
+      filterList: [],
     };
   },
   methods: {
     changeTab(tabNum) {
       this.selectedTabNum = tabNum;
+    },
+    applyFilter() {
+      // console.log(this.checkedFilter);
+
+      let filterStr = JSON.stringify(this.checkedFilter)
+        .replace(/\[/gi, "!q@w#e")
+        .replace(/\]/gi, "q!w@e#");
+      console.log(filterStr);
+
+      console.log(JSON.stringify(this.checkedFilter));
+      axios
+        .get(`/survey/${this.surveyId}/answer`, {
+          params: {
+            filterDataList: filterStr,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    },
+    clickFilterDiv(filterIdx) {
+      this.filterList[filterIdx].isSelected =
+        !this.filterList[filterIdx].isSelected;
     },
   },
   computed: {
@@ -92,6 +183,43 @@ export default {
       }
       return true;
     },
+  },
+  created() {
+    axios.get(`/survey/${this.surveyId}/filters`).then((res) => {
+      console.log(res);
+
+      res.data.filterQuestionList.forEach((el) => {
+        let title = el.title;
+        let content = JSON.parse(el.content);
+
+        let filterNames = [];
+
+        Object.keys(content).forEach((key) => {
+          filterNames.push(content[key]);
+        });
+
+        this.checkedFilter.push({
+          filterKind: title,
+          filterValue: [],
+        });
+
+        this.filterList.push({
+          name: title,
+          isSelected: false,
+          filterNames: filterNames,
+        });
+      });
+    });
+    // this.filterList.forEach((el) => {
+    //   let filter = {
+    //     filterKind: el.name,
+    //     filterValue: [],
+    //   };
+
+    //   this.checkedFilter.push(filter);
+    // });
+
+    console.log(this.checkedFilter);
   },
 };
 </script>
@@ -184,5 +312,82 @@ ul.tabs li:hover {
 
 .el-card {
   transition: 0s !important;
+}
+</style>
+
+<style scoped>
+.filte-div {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+.filter-title {
+  display: flex;
+  justify-content: space-between;
+  /* padding-left: 2%; */
+  /* margin: 2%; */
+  background-color: #dde0e7;
+  width: 100%;
+  /* border-style: solid; */
+  cursor: pointer;
+}
+.filter {
+  /* display: flex;
+  flex-direction: column;
+  align-items: center; */
+  width: 90% !important;
+  /* border: 0px; */
+  /* border-style: solid; */
+  /* padding: 5%; */
+}
+.filter-content {
+  padding: 5%;
+  width: 90%;
+}
+
+.filter-content div {
+  cursor: pointer;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.filter-content label {
+  cursor: pointer;
+}
+
+.filter-content input {
+  cursor: pointer;
+}
+
+.filter-div {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.filter-list {
+  width: 100%;
+
+  /* justify-content: center; */
+}
+
+.filter-list i {
+  display: flex;
+  align-items: center;
+}
+
+.filter {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.button-div {
+  display: flex;
+  justify-content: flex-end;
+  margin: 5%;
 }
 </style>
