525941f014e344979b11544515a8e2d80aeee359
kangazy7 <hg_kang7@Naver.com>
2021-11-05 15:11:52 +0900

    [#29] fix: 차트크기 조절

--- frontend/src/components/detail/SurveyAnalysis.vue
+++ frontend/src/components/detail/SurveyAnalysis.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <div class="el-card box-card is-always-shadow" style="width:90%; height:90%; margin-left:5%; margin-top:3%; margin-bottom:2%">
-      <div style="margin-top: 2%; margin-left: 2%">
+    <div class="el-card box-card is-always-shadow" style="width:90%; height:80%; margin-left:5%; margin-top:3%; margin-bottom:2%">
+      <div style="margin-top: 2%; margin-left: 2%; ">
         <h2>카테고리별 데이터</h2>
-      </div>
-      <RadarChart style="margin-left: 30%; width:24%; height:24%"/>
+      </div>     
+      <RadarChart style="margin-left: 30%;margin-top: -5%; width:24%; height:24%"/> 
     </div>
-    <div class="el-card box-card is-always-shadow" style="width:90%; height:90%; margin-left:5%; margin-top:2%; margin-bottom:2%">
+    <div class="chartWrapper el-card box-card is-always-shadow" style="width:90%; height:80%; margin-left:5%; margin-top:2%; margin-bottom:2%">
       <div style="margin-top: 2%; margin-left: 2%">
         <h2>문항별 데이터</h2>
       </div>
-      <BarChart/>
+      <div class="">
+        <BarChart style="width:1500px; height:40%"/>
+
+      </div>
     </div>
     <!-- <div>
       <canvas id="Bar" width="400" height="400"></canvas>
@@ -39,3 +42,20 @@ export default {
   },
 };
 </script>
+<style scoped>
+.chartWrapper {
+  position: relative;
+}
+
+.chartWrapper > canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+  pointer-events: none;
+}
+
+.chartAreaWrapper {
+  width: 15000px;
+  overflow-x: scroll;
+}
+</style>
