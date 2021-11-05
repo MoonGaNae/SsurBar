4217af68033eeae1cc36681afdb463727c001560
DESKTOP-KVCQHCD\multicampus <hyeonmean@gmail.com>
2021-11-05 17:42:28 +0900

    [#32] feat: 피드백 및 차트 디자인 수정

--- frontend/src/components/charts/RadarChart.vue
+++ frontend/src/components/charts/RadarChart.vue
@@ -1,47 +1,52 @@
 <script>
-  import { Radar } from 'vue-chartjs'
+import { Radar } from "vue-chartjs";
 
-  export default {
-    extends: Radar,
-    data () {
-      return {
-        datacollection: {
-          labels: ['1번 항목', '2번 항목', '3번 항목', '4번 항목','5번 항목'],
-          datasets: [
-            {
-              label: 'Dataset',
-              backgroundColor: "rgba(255, 10, 13, 0.2)",
-              pointBackgroundColor: 'white',
-              borderWidth: 5,
-              pointBorderColor: '#249EBF',
-              data: [5,6,7,4,4],
-              
-            },
-            // {
-            //   label: 'Dataset2',
-            //   fill:true,
-            //   backgroundColor: "rgba(25, 10, 13, 0.4)",
-            //   pointBackgroundColor: 'white',
-            //   borderWidth: 1,
-            //   pointBorderColor: '#249EBF',
-            //   data: [8,6,8,8,8],
-              
-            // }
-          ]
+export default {
+  extends: Radar,
+  data() {
+    return {
+      datacollection: {
+        labels: ["1번 항목", "2번 항목", "3번 항목", "4번 항목", "5번 항목"],
+        datasets: [
+          {
+            label: "Dataset",
+            backgroundColor: "rgba(255, 10, 13, 0.2)",
+            pointBackgroundColor: "white",
+            borderWidth: 5,
+            pointBorderColor: "#249EBF",
+            data: [5, 1, 5, 2, 3],
+          },
+          // {
+          //   label: 'Dataset2',
+          //   fill:true,
+          //   backgroundColor: "rgba(25, 10, 13, 0.4)",
+          //   pointBackgroundColor: 'white',
+          //   borderWidth: 1,
+          //   pointBorderColor: '#249EBF',
+          //   data: [8,6,8,8,8],
+
+          // }
+        ],
+      },
+      options: {
+        legend: {
+          display: false,
         },
-        options: {
+        maintainAspectRatio: false,
         scale: {
           angleLines: {
-            display: false
+            display: false,
           },
           ticks: {
-            suggestedMin: 1,
-            suggestedMax: 8
-          }}}
-      };
-    },
-    mounted () {
-      this.renderChart(this.datacollection, this.options)
-    }
-  }
+            suggestedMin: 0,
+            suggestedMax: 5,
+          },
+        },
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.datacollection, this.options);
+  },
+};
 </script>
--- frontend/src/components/detail/SurveyAnalysis.vue
+++ frontend/src/components/detail/SurveyAnalysis.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>
-    <div class="el-card box-card is-always-shadow" style="width:90%; height:90%; margin-left:5%; margin-top:3%; margin-bottom:2%">
-      <div style="margin-top: 2%; margin-left: 2%">
+  <div class="chart-container">
+    <div
+      class="el-card box-card is-always-shadow"
+      style="
+        width: 90%;
+        height: 95%;
+        margin-left: 5%;
+        margin-top: 3%;
+        margin-bottom: 2%;
+      "
+    >
+      <div class="chart-title">
         <h2>카테고리별 데이터</h2>
       </div>
-      <RadarChart style="margin-left: 30%; width:24%; height:24%"/>
+      <div class="chart-div">
+        <RadarChart />
+      </div>
     </div>
-    <div class="el-card box-card is-always-shadow" style="width:90%; height:90%; margin-left:5%; margin-top:2%; margin-bottom:2%">
-      <div style="margin-top: 2%; margin-left: 2%">
+    <div
+      class="el-card box-card is-always-shadow bar-chart-container"
+      style="
+        width: 90%;
+        height: 100%;
+        margin-left: 5%;
+        margin-top: 2%;
+        margin-bottom: 2%;
+      "
+    >
+      <div class="chart-title">
         <h2>문항별 데이터</h2>
       </div>
-      <BarChart/>
+      <div class="bar-chart-div-parent">
+        <div class="bar-chart-div" :style="{ width: widthTemp }">
+          <BarChart />
+        </div>
+      </div>
     </div>
     <!-- <div>
       <canvas id="Bar" width="400" height="400"></canvas>
@@ -19,12 +43,11 @@
       <radar-chart></radar-chart>
     </div> -->
   </div>
-
 </template>
 
 <script>
-import BarChart from '../charts/BarChart.vue'
-import RadarChart from '../charts/RadarChart.vue'
+import BarChart from "../charts/BarChart.vue";
+import RadarChart from "../charts/RadarChart.vue";
 
 export default {
   name: "SurveyAnalysis",
@@ -34,8 +57,98 @@ export default {
   },
   data() {
     return {
-      datacollection: null
-    }
+      datacollection: null,
+      count: 25,
+      widthTemp: "30px",
+    };
+  },
+  mounted() {
+    let barChartDiv = document.getElementsByClassName("bar-chart-div")[0];
+    barChartDiv.style.width = 1000;
+  },
+  created() {
+    this.widthTemp = this.count * 100 + "px";
   },
 };
 </script>
+
+<style>
+/* #bar-chart {
+  height: 100% !important;
+  width: 100% !important;
+} */
+</style>
+
+<style scoped>
+.chart-div div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+
+.bar-chart-div div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 3000px;
+}
+.chart-container {
+  overflow: scroll;
+  overflow-x: hidden;
+  height: 100%;
+}
+
+.chart-container::-webkit-scrollbar {
+  width: 1vh;
+}
+.chart-container::-webkit-scrollbar-track {
+  background-color: #dde0e7;
+}
+
+.chart-container::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  background-color: #9cbbff;
+}
+
+.chart-div {
+  display: flex;
+  width: 100%;
+  height: 90%;
+  padding: 3%;
+  justify-content: center;
+  align-items: center;
+  /* overflow: scroll; */
+}
+
+.bar-chart-div {
+  display: flex;
+  /* width: 3000px; */
+  height: 90%;
+  padding: 3%;
+  justify-content: center;
+  align-items: center;
+  /* overflow: scroll; */
+}
+
+.chart-title {
+  margin-top: 2%;
+  margin-left: 2%;
+  height: 10%;
+}
+
+.bar-chart-div-parent {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: flex-start;
+  /* overflow: scroll; */
+}
+
+.bar-chart-container {
+  overflow: scroll;
+  overflow-y: hidden;
+}
+</style>
