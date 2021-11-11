a3f2abfe8441dcfd88348b20dfd525381a46be3f
MoonGaNae <anstjd9115@gmail.com>
2021-11-11 15:57:05 +0900

    [#38] wip : JWT 토큰을 위한 axios 인터셉터 추가

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,11 +1,42 @@
 import axios from "axios";
+import store from "@/store/index.js";
 
 // axios 객체 생성
-export default axios.create({
+const _axios = axios.create({
   baseURL: "http://k5f001.p.ssafy.io:8080/api/v1",
-  //baseURL: "http://localhost:8080/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
   headers: {
     "Content-type": "application/json",
   },
-  //withCredentials: true,
+  withCredentials: true,
 });
+
+_axios.interceptors.request.use(
+  (config) => {
+    if(store.state.user.token){
+      config.headers['authorization'] = store.state.user.token;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+)
+
+_axios.interceptors.response.use(
+  (response) => {
+    if(response.headers.authorization){
+      store.commit('user/SET_TOKEN', response.headers.authorization)
+    }
+    return Promise.resolve(response)
+  },
+  (err) => {
+    if(err.response.status === 401){
+      // 로그인페이지 보내기
+    }
+
+    return Promise.reject(err);
+  }
+)
+
+export default _axios;
