e718d0f2c7bc04ddfdface7ed4980424ddf3369d
MoonGaNae <anstjd9115@gmail.com>
2021-11-11 15:58:19 +0900

    [#38] wip : user API store 연동

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,13 +8,14 @@ import team from "@/store/modules/team.js";
 import filterQuestion from "@/store/modules/filterQuestion.js";
 import list from "@/store/modules/list.js";
 import analysis from "@/store/modules/analysis.js";
+import user from "@/store/modules/user.js";
 
 Vue.use(Vuex);
 
 Vue.prototype.$mainUrl = "http://localhost:8080/";
 
 export default new Vuex.Store({
-  modules: { question, template, survey, team, filterQuestion, list, analysis },
+  modules: { question, template, survey, team, filterQuestion, list, analysis , user},
   state: {},
   mutations: {},
   actions: {},
