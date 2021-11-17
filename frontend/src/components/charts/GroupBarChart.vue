c78efd96fe9a3db0cf75d46635077cd4dd51423e
DESKTOP-KVCQHCD\multicampus <hyeonmean@gmail.com>
2021-11-17 20:38:59 +0900

    [#58] feat: 비교페이지 차트 수정

---
+++ frontend/src/components/charts/GroupBarChart.vue
@@ -0,0 +1,88 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+import { mapState } from "vuex";
+
+export default {
+  extends: HorizontalBar,
+  name: "BarChart",
+  watch: {
+    comparisonDataSets() {
+      this.makeGroupBarChart();
+    },
+  },
+  computed: {
+    ...mapState("analysis", ["barLabels", "barDataSets"]),
+    ...mapState("analysis", [
+      "answerDataList",
+      "comparisonLabels",
+      "comparisonDataSets",
+    ]),
+  },
+  data() {
+    return {
+      datacollection: {
+        labels: [],
+        datasets: [],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              beginAtZero: true,
+              ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 5,
+                stepSize: 0.5,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 5,
+                stepSize: 0.5,
+              },
+              beginAtZero: true,
+            },
+          ],
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+      },
+    };
+  },
+  methods: {
+    makeGroupBarChart() {
+      let labels = this.comparisonLabels;
+      let dataSets = [];
+
+      this.comparisonDataSets.forEach((el, idx) => {
+        let dataSet = {
+          label: this.comparisonLabels[idx],
+          data: el.data,
+          borderColor: el.backgroundColor,
+          backgroundColor: el.backgroundColor,
+        };
+
+        dataSets.push(dataSet);
+      });
+      this.datacollection.datasets = dataSets;
+      this.datacollection.labels = labels;
+
+      this.renderChart(this.datacollection, this.options);
+    },
+  },
+  mounted() {
+    this.makeGroupBarChart();
+  },
+};
+</script>
